694c5f6866488c10f7653be2765ee5e319de1372
brunagabrielly2301 <brunagabrielly230120@gmail.com>
2026-09-14 16:19:58 -0300

    test: adiciona falha intencional no codigo

--- server.js
+++ server.js
@@ -1,10 +1,2 @@
-const express = require ('express') ;
-const app = express() ;
-const port = 3000 ;
-
-app.get('/', (req, res) => {
-    res.send('aplicaçao rodando em container docker!');
-});
-app.listen(port, ()=> {
-    console.log('servidor rodando na porta ${port}');
-});
+const variavelNaoUtilizada = 123;
+throw new Error('Falha intencional de teste no CI');
